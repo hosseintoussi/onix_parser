68baeb10b059dd6b16bab5d484dcec967e463f5c
Hosseintoussi <sltoussi@gmail.com>
2020-01-03 19:02:36 +0100

    use hash bracket over dup

--- lib/onix_parser/attributes/types/collection.rb
+++ lib/onix_parser/attributes/types/collection.rb
@@ -4,19 +4,20 @@ module OnixParser
       class Collection
         def coerce(values, options = {})
           collection = values.is_a?(::Array) ? values : [values]
-          klass = item_klass(options.delete(:of))
+          options = Hash[options]
+          type = item_type(options.delete(:of))
 
           collection.map! do |value|
-            Types.coerce(klass, value, options)
+            Types.coerce(type, value, options)
           end
         end
 
         private
 
-        def item_klass(klass)
-          return klass unless klass.is_a? ::String
+        def item_type(type)
+          return type unless type.is_a? ::String
 
-          Object.const_get(klass)
+          Object.const_get(type)
         end
       end
     end
